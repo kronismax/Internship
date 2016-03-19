db0f5916c2862c79c79cceb143e852a6f3fbade4
max <lituchii.maksim@mail.ru>
2016-03-19 12:40:18 +0200

    comment hotfix

--- app/src/main/java/lituchiy/max/internship/MainActivity.java
+++ app/src/main/java/lituchiy/max/internship/MainActivity.java
@@ -1,27 +1,21 @@
 package lituchiy.max.internship;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.Arrays;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import lituchiy.max.internship.adapter.ImageAdapter;
 
 //[Comment] Wrong toolbar and status bar color
 public class MainActivity extends AppCompatActivity {
@@ -30,27 +24,30 @@ public class MainActivity extends AppCompatActivity {
             "https://lh5.googleusercontent.com/-0BDXkYmckbo/URquhKFW84I/AAAAAAAAAbs/ogQtHCTk2JQ/s1024/Closed%252520Door.jpg",
             "https://lh3.googleusercontent.com/-PyggXXZRykM/URquh-kVvoI/AAAAAAAAAbs/hFtDwhtrHHQ/s1024/Colorado%252520River%252520Sunset.jpg",
             "https://lh4.googleusercontent.com/-e9NHZ5k5MSs/URqvMIBZjtI/AAAAAAAAAbs/1fV810rDNfQ/s1024/Yosemite%252520Tree.jpg",
-    }; //[Comment] Hardcode. Move this strings into <string-array />
+    }; //[Comment] Hardcode. Move this strings into <string-array /> DONE
 
-    @Bind(R.id.toolbar) Toolbar mToolbar;
-    @Bind(R.id.recycler_view) RecyclerView mRecyclerView; //[Comment] Wrong formatter
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.recycler_view)
+    RecyclerView mRecyclerView; //[Comment] Wrong formatter DONE
 
-    @OnClick({ R.id.createdTv,
+    @OnClick({R.id.createdTv,
             R.id.createdDateTv,
             R.id.titleTv,
             R.id.statusTv,
-            R.id.registeredTv ,
+            R.id.registeredTv,
             R.id.registeredDateTv,
             R.id.assignedTv,
             R.id.assignedDateTv,
             R.id.responsibleTv,
             R.id.responsibleNameTv,
-            R.id.descriptionTv })
+            R.id.descriptionTv})
     public void showToast(View view) {
         Toast.makeText(this,
                 ((TextView) view).getText(), Toast.LENGTH_SHORT)
                 .show();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(layoutManager);
 
-        ImageAdapter imageAdapter = new ImageAdapter(Arrays.asList(imageUrls), this);
+        ImageAdapter imageAdapter = new ImageAdapter(Arrays.asList(getResources().getStringArray(R.array.image_urls)), this);
         mRecyclerView.setAdapter(imageAdapter);
     }
 
@@ -84,58 +81,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
-    //[Comment] ImageAdapter shouldn't be internal class
-    public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
-        private List<String> mImageList;
-        private Context mContext;
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-
-            @Bind(R.id.imageView) public ImageView mImageView;
-
-            public ViewHolder(View v) {
-                super(v);
-                ButterKnife.bind(this, v);
-            }
-        }
-
-        public ImageAdapter(List<String> mImageList, Context context) {
-            mContext = context;
-            this.mImageList = mImageList; // [Comment] mImageList = imageList
-        }
-
-        @Override
-        public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_item, parent, false);
-            return new ViewHolder(v);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, final int position) {
-
-            Picasso.with(mContext).load(mImageList.get(position)).error(R.drawable.marshmallow).into(holder.mImageView);
-
-            holder.mImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(mContext, "" + position, Toast.LENGTH_SHORT).show();
-                }
-            });
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mImageList.size();
-        }
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish(); //[Comment] You don't need this. You have single activity.
-    }
+    //[Comment] ImageAdapter shouldn't be internal class DONE
 
 }
---
+++ app/src/main/java/lituchiy/max/internship/adapter/ImageAdapter.java
@@ -0,0 +1,63 @@
+package lituchiy.max.internship.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import lituchiy.max.internship.R;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private List<String> mImageList;
+    private Context mContext;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.imageView)
+        public ImageView mImageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+    public ImageAdapter(List<String> imageList, Context context) {
+        mContext = context;
+        this.mImageList = imageList; // [Comment] mImageList = imageList
+    }
+
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+        Picasso.with(mContext).load(mImageList.get(position)).error(R.drawable.marshmallow).into(holder.mImageView);
+
+        holder.mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageList.size();
+    }
+}
