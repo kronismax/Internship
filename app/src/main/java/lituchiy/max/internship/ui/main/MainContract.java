ed49a223bcd591a0fc6cbff7b3528cc8a38422b7
max <lituchii.maksim@mail.ru>
2016-05-26 23:49:38 +0300

    add interfaces to view layer

---
+++ app/src/main/java/lituchiy/max/internship/ui/main/MainContract.java
@@ -0,0 +1,12 @@
+package lituchiy.max.internship.ui.main;
+
+
+import java.util.List;
+
+import lituchiy.max.internship.data.model.Appeal;
+
+public interface MainContract {
+
+    void refreshAppealList(List<Appeal> mAppealList);
+
+}
